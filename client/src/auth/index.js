859210f762d8e435dcebd4cefbc4873a93769169
Kevin Calle <kevin.calle@stonybrook.edu>
2021-11-05 19:53:23 -0400

    Login and Logout

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -8,7 +8,9 @@ console.log("create AuthContext: " + AuthContext);
 // THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
 export const AuthActionType = {
     GET_LOGGED_IN: "GET_LOGGED_IN",
-    REGISTER_USER: "REGISTER_USER"
+    REGISTER_USER: "REGISTER_USER",
+    LOGIN_USER: "LOGIN_USER",
+    LOGOUT_USER: "LOGOUT_USER"
 }
 
 function AuthContextProvider(props) {
@@ -37,35 +39,94 @@ function AuthContextProvider(props) {
                     loggedIn: true
                 })
             }
+            case AuthActionType.LOGIN_USER: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true
+                })
+            }
+            case AuthActionType.LOGOUT_USER: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false
+                })
+            }
             default:
                 return auth;
         }
     }
 
     auth.getLoggedIn = async function () {
-        const response = await api.getLoggedIn();
-        if (response.status === 200) {
-            authReducer({
-                type: AuthActionType.SET_LOGGED_IN,
-                payload: {
-                    loggedIn: response.data.loggedIn,
-                    user: response.data.user
-                }
-            });
+        try {
+            const response = await api.getLoggedIn();
+            if (response.status === 200) {
+                authReducer({
+                    type: AuthActionType.GET_LOGGED_IN,
+                    payload: {
+                        loggedIn: response.data.loggedIn,
+                        user: response.data.user
+                    }
+                });
+            }
+        }
+        catch(err){
+            console.error(err);
         }
     }
 
     auth.registerUser = async function(userData, store) {
-        const response = await api.registerUser(userData);      
-        if (response.status === 200) {
-            authReducer({
-                type: AuthActionType.REGISTER_USER,
-                payload: {
-                    user: response.data.user
-                }
-            })
-            history.push("/");
-            store.loadIdNamePairs();
+        try{
+            const response = await api.registerUser(userData);      
+            if (response.status === 200) {
+                authReducer({
+                    type: AuthActionType.REGISTER_USER,
+                    payload: {
+                        user: response.data.user
+                    }
+                })
+                history.push("/");
+                store.loadIdNamePairs();
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    auth.loginUser = async function(userData, store) {
+        try {
+            const response = await api.loginUser(userData);
+            if (response.status === 200) {
+                authReducer( {
+                    type: AuthActionType.LOGIN_USER,
+                    payload: {
+                        user: response.data.user
+                    }
+                })
+                history.push("/");
+                store.loadIdNamePairs();
+            }
+            else {
+                console.log("Unauthorized");
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    auth.logoutUser = async function() {
+        try{
+            const response = await api.logoutUser();
+            if (response.status === 200) {
+                authReducer( {
+                    type: AuthActionType.LOGOUT_USER,
+                })
+                history.push("/");
+            }
+        }
+        catch (err) {
+            console.error(err);
         }
     }
 
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,17 +3,22 @@ const User = require('../models/user-model')
 const bcrypt = require('bcryptjs')
 
 getLoggedIn = async (req, res) => {
-    auth.verify(req, res, async function () {
-        const loggedInUser = await User.findOne({ _id: req.userId });
-        return res.status(200).json({
-            loggedIn: true,
-            user: {
-                firstName: loggedInUser.firstName,
-                lastName: loggedInUser.lastName,
-                email: loggedInUser.email
-            }
-        }).send();
-    })
+    try {
+        auth.verify(req, res, async function () {
+            const loggedInUser = await User.findOne({ _id: req.userId });
+            return res.status(200).json({
+                loggedIn: true,
+                user: {
+                    firstName: loggedInUser.firstName,
+                    lastName: loggedInUser.lastName,
+                    email: loggedInUser.email
+                }
+            }).send();
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
 }
 
 registerUser = async (req, res) => {
@@ -78,7 +83,68 @@ registerUser = async (req, res) => {
     }
 }
 
+loginUser = async (req, res) => {
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+        //const {email, password} = req.body;
+        console.log(email);
+
+        // ENSURE REQUIRED FIELDS
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+
+        // VERIFY USER
+        const existingUser = await User.findOne({ email: email });
+        if (!existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "Wrong email or password."
+                })
+        }
+        // VERIFY PASSWORD
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+        if (!passwordCorrect) {
+            return res
+                   .status(401)
+                   .json({
+                       errorMessage: "Wrong email or password."
+                   })
+        }
+        // LOGIN
+        const token = auth.signToken(existingUser);
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: existingUser.firstName,
+                lastName: existingUser.lastName,
+                email: existingUser.email
+            }
+        }).send();
+
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+logoutUser = async (req, res) => {
+    return res.clearCookie("token").status(200).json({ success: true })
+}
+
 module.exports = {
     getLoggedIn,
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
 }
